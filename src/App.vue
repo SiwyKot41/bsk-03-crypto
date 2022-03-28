1329179f61c930001951a6423bdf32c915c99dca
SiwyKot41 <frutkowski80@gmail.com>
2022-03-28 11:42:11 +0200

    Add router

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,30 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import Crypto from "./components/Crypto.vue";
+import Generator from "./components/Generator.vue";
+import {computed, ref} from "vue";
+
+const routes = {
+  '/': Crypto,
+  '/generator': Generator
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/']
+})
+
 </script>
 
 <template>
-  <Crypto/>
+  <a href="#/">Crypto</a> |
+  <a href="#/generator">Generator</a> |
+  <component :is="currentView"/>
 </template>
 
 <style>
