ac16c28f4cb5b4e567dd43ecca6942b6059f29db
Monikaa1909 <monikaa1909@gmail.com>
2022-04-25 22:13:33 +0200

    first iteration done to 10th step (last =thing done was the xor operation)

--- src/components/DES.vue
+++ src/components/DES.vue
@@ -24,6 +24,16 @@ const errorMessageNoFileInput = ref('')
 const selectedFile = ref(null)
 const blocksArray = ref([]) //tu jest tablica gdzie będą wszystkie bloki
 
+let key = [
+  0, 1, 0, 1, 1, 0, 1, 0,
+  1, 0, 1, 1, 1, 0, 0, 0,
+  0, 1, 0, 1, 1, 1, 1, 1,
+  0, 0, 0, 1, 1, 0, 1, 0,
+  1, 0, 1, 1, 0, 0, 1, 0,
+  0, 0, 0, 1, 0, 0, 1, 0,
+  1, 0, 1, 1, 1, 0, 0, 0,
+  0, 0, 1, 1, 1, 0, 1, 0
+]
 let dataBlock = [
     1, 0, 1, 1, 1, 0, 1, 0,
     1, 0, 1, 1, 1, 0, 0, 0,
@@ -32,9 +42,9 @@ let dataBlock = [
     1, 0, 1, 1, 0, 0, 1, 0,
     0, 0, 0, 1, 0, 0, 1, 0,
     1, 1, 1, 1, 1, 0, 0, 1,
-    1, 1, 1, 1, 1, 0, 1, 0]
-
-const initialPermutation = [
+    1, 1, 1, 1, 1, 0, 1, 0
+]
+let initialPermutation = [
   58, 50, 42, 34, 26, 18, 10, 2,
   60, 52, 44, 36, 28, 20, 12, 4,
   62, 54, 46, 38, 30, 22, 14, 6,
@@ -42,7 +52,44 @@ const initialPermutation = [
   57, 49, 41, 33, 25, 17, 9, 1,
   59, 51, 43, 35, 27, 19, 11, 3,
   61, 53, 45, 37, 29, 21, 13, 5,
-  63, 55, 47, 39, 31, 23, 15, 7]
+  63, 55, 47, 39, 31, 23, 15, 7
+]
+let permutedChoice = [
+  57, 49, 41, 33, 25, 17, 9,
+  1, 58, 50, 42, 34, 26, 18,
+  10, 2, 59, 51, 43, 35, 27,
+  19, 11, 3, 60, 52, 44, 36,
+  63, 55, 47, 39, 31, 23, 15,
+  7, 62, 54, 46, 38, 30, 22,
+  14, 6, 61, 53, 45, 37, 29,
+  21, 13, 5, 28, 20, 12, 4
+]
+let permutedChoice2 = [
+  14, 17, 11, 24, 1, 5,
+  3, 28, 15, 6, 21, 10,
+  23, 19, 12, 4, 26, 8,
+  16, 7, 27, 20, 13, 2,
+  41, 52, 31, 37, 47, 55,
+  30, 40, 51, 45, 33, 48,
+  44, 49, 39, 56, 34, 53,
+  46, 42, 50, 36, 29, 32
+]
+let shiftTableForEachIteration = [
+  1, 1, 2, 2,
+  2, 2, 2, 2,
+  1, 2, 2, 2,
+  2, 2, 2, 1
+]
+const extensionArray = [
+  32, 1, 2, 3, 4, 5,
+  4, 5, 6, 7, 8, 9,
+  8, 9, 10, 11, 12, 13,
+  12, 13, 14, 15, 16, 17,
+  16, 17, 18, 19, 20, 21,
+  20, 21, 22, 23, 24, 25,
+  24, 25, 26, 27, 28, 29,
+  28, 29, 30, 31, 32, 1
+]
 
 function showBlock(block) {
   let bits = '';
@@ -53,6 +100,8 @@ function showBlock(block) {
 }
 
 function startDES(event){
+  let leftDataBlock, rightDataBlock, CKeyBlock, DKeyBlock, xorBlock
+
   // tu pewnie będzie: for(block in blocks):
 
   console.log("Blok początkowy:")
@@ -62,18 +111,57 @@ function startDES(event){
   console.log("Blok po permutacji 'initial permutation': ")
   showBlock(dataBlock)
 
-  let leftDataBlock = []
-  let rightDataBlock = []
-  leftDataBlock = makeLeftBlock(dataBlock)
-  rightDataBlock = makeRightBlock(dataBlock)
+  leftDataBlock = dataBlock.slice(0, dataBlock.length/2)
+  rightDataBlock = dataBlock.slice(dataBlock.length/2)
   console.log("Lewy blok: ")
   showBlock(leftDataBlock)
   console.log("Prawy blok: ")
   showBlock(rightDataBlock)
+
+  console.log("Klucz początkowy: ")
+  // zamieniam klucz na zwykłe liczby od 1 do 64 tylko po to żeby łatwiej sprawdzać poprawność permutacji
+  // for(let i = 0; i < 64; i++) {
+  //   key[i] = i+1
+  // }
+  showBlock(key)
+
+  key = makePermutedChoice(key)
+  console.log("Przetworzony klucz 56-bitowy permutacją 'permuted choice': ")
+  showBlock(key)
+
+  CKeyBlock = key.slice(0, key.length/2)
+  DKeyBlock = key.slice(key.length/2)
+  console.log("Pierwsza część klucza C: ")
+  showBlock(CKeyBlock)
+  console.log("Druga część klucza D: ")
+  showBlock(DKeyBlock)
+
+  CKeyBlock = makeLeftShift(CKeyBlock, shiftTableForEachIteration[0])
+  console.log("Pierwsza część klucza C po 1. przesunięciu w lewo: ")
+  showBlock(CKeyBlock)
+  DKeyBlock = makeLeftShift(DKeyBlock, shiftTableForEachIteration[0])
+  console.log("Pierwsza część klucza D po 1. przesunięciu w lewo: ")
+  showBlock(DKeyBlock)
+
+  key = CKeyBlock.concat(DKeyBlock)
+  console.log("Połączone części klucza C i D: ")
+  showBlock(key)
+
+  key = makePermutedChoice2(key)
+  console.log("Przetworzony klucz 48-bitowy permutacją 'permuted choice 2': ")
+  showBlock(key)
+
+  rightDataBlock = makeExtensionArrayPermutation(rightDataBlock)
+  console.log("Przetworzona prawa część bloku wejściowego za pomocą 'extension array': ")
+  showBlock(rightDataBlock)
+
+  xorBlock = makeXORforBlocks(key, rightDataBlock)
+  console.log("48-bitowy ciąg uzyskany za pomocą operacji XOR na 48-bitowym kluczu i 48-bitowej wersji prawego bloku: ")
+  showBlock(xorBlock)
 }
 
 function makeInitialPermutation(block) {
-  // tworzymy tablicę pomocniczą przechowującą aktualne wartości 64-bitowego bloku:
+  // stworzenie tablicy pomocniczej przechowującej aktualne wartości 64-bitowego bloku:
   let tmp = [];
   for (let i = 0; i < block.length; i++) {
     tmp[i] = block[i]
@@ -87,22 +175,67 @@ function makeInitialPermutation(block) {
   return block
 }
 
-function makeLeftBlock(block) {
-  let leftBlock = []
-  for (let i = 0; i < 32; i++) {
-    leftBlock[i] = block[i]
+function makePermutedChoice(block) {
+  // stworzenie nowej tablicy do przechowywania wartości ze zredukowanej tablicy wejściowej
+  // za pomocą permutacji 'permuted choice':
+  let reducedBlock = []
+  for (let i = 0; i < permutedChoice.length; i++) {
+    reducedBlock[i] = block[permutedChoice[i]-1]
   }
-  return leftBlock
+  return reducedBlock
 }
 
-function makeRightBlock(block) {
-  let rightBlock = []
-  for (let i = 0; i < 32; i++) {
-    rightBlock[i] = block[i + 32]
+function makePermutedChoice2(block) {
+  // stworzenie nowej tablicy do przechowywania wartości ze zredukowanej tablicy wejściowej
+  // za pomocą permutacji 'permuted choice 2':
+  let reducedBlock = []
+  for (let i = 0; i < permutedChoice2.length; i++) {
+    reducedBlock[i] = block[permutedChoice2[i]-1]
   }
-  return rightBlock
+  return reducedBlock
 }
 
+function makeLeftShift(block, numberOfBits) {
+  // zapamiętanie pierwszych n bitów bloku wejściowego, gdzie n to liczba bitów o które przesuwamy:
+  let tmp = []
+  for (let i = 0; i < numberOfBits; i++) {
+    tmp[i] = block[i]
+  }
+
+  // przesunięcie wszystkich bitów o n bitów w lewo, poza n ostatnimi, gdzie n to liczba bitów o które przesuwamy:
+  for (let i = 0; i < block.length - numberOfBits; i++) {
+    block[i] = block[i + numberOfBits]
+  }
+
+  // przypisanie do bloku wyjściowego na końcu n zapamiętanych w kroku wyżej pierwszych bitów bloku wejściowego, gdzie n
+  // to liczba bitów o które przesuwamy:
+  for (let i = 0; i < numberOfBits; i++) {
+    block[block.length - numberOfBits + i] = tmp[i]
+  }
+
+  return block
+}
+
+function makeExtensionArrayPermutation(block) {
+  let extensionBlock = []
+  for (let i = 0; i < extensionArray.length; i++) {
+    extensionBlock[i] = block[extensionArray[i]-1]
+  }
+  return extensionBlock
+}
+
+function XOR(p, q) {
+  if (p === q) return 0;
+  else return 1;
+}
+
+function makeXORforBlocks(firstBlock, secondBlock) {
+  let xorBlock = []
+  for (let i = 0; i < firstBlock.length; i++) {
+    xorBlock[i] = XOR(firstBlock[i], secondBlock[i])
+  }
+  return xorBlock
+}
 
 function onFileSelected(event) {
   selectedFile.value = event.target.files[0]
