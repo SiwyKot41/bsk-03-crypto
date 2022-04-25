5df2bd95cd720de6fb96439fd8dafe432442fb3a
Monikaa1909 <monikaa1909@gmail.com>
2022-04-25 15:11:21 +0200

    initial permutation works

--- src/components/DES.vue
+++ src/components/DES.vue
@@ -9,7 +9,7 @@
         <p></p>
         <div class="errMsg"> {{ errorMessageNoFileInput }}</div>
           <div class="buttonContainer">
-            <button @click="showTable">show table</button>
+            <button @click="startDES">Start DES</button>
           </div>
       </div>
     </div>
@@ -24,7 +24,7 @@ const errorMessageNoFileInput = ref('')
 const selectedFile = ref(null)
 const blocksArray = ref([])
 
-const dataBlock = ref([
+let dataBlock = ref([
     1, 0, 1, 1, 1, 0, 1, 0,
     1, 0, 1, 1, 1, 0, 0, 0,
     0, 1, 0, 1, 1, 0, 1, 1,
@@ -39,24 +39,39 @@ const initialPermutation = ref([
   60, 52, 44, 36, 28, 20, 12, 4,
   62, 54, 46, 38, 30, 22, 14, 6,
   64, 56, 48, 40, 32, 24, 16, 8,
-  57, 49, 41, 33, 25, 17, 9,1,
+  57, 49, 41, 33, 25, 17, 9, 1,
   59, 51, 43, 35, 27, 19, 11, 3,
   61, 53, 45, 37, 29, 21, 13, 5,
   63, 55, 47, 39, 31, 23, 15, 7])
 
-function showTable(event) {
+function showTable(arr) {
   let table = '';
-  for (let i = 0; i < dataBlock.value.length; i++) {
-    table += dataBlock.value[i] + " "
+  for (let i = 0; i < arr.length; i++) {
+    table += arr[i] + " "
   }
   console.log(table);
 }
 
-function makeInitialPermutation(event) {
-  let tmp = dataBlock.value
-  for (let i = 0; i < dataBlock.value.length; i++) {
+function startDES(event){
+  makeInitialPermutation()
+}
 
+function makeInitialPermutation() {
+  // tworzymy tablicę pomocniczą przechowującą aktualne wartości 64-bitowego bloku:
+  let tmp = [];
+  for (let i = 0; i < dataBlock.value.length; i++) {
+    tmp[i] = dataBlock.value[i]
   }
+
+  console.log("Blok początkowy:")
+  showTable(dataBlock.value);
+
+  for (let i = 0; i < dataBlock.value.length; i++) {
+    dataBlock.value[i] = tmp[initialPermutation.value[i]-1]
+  }
+
+  console.log("Blok po permutacji 'initial permutation': ")
+  showTable(dataBlock.value);
 }
 
 
