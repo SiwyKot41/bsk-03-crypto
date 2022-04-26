cfe7e6f3f3fe90a9857158f61f1622f111a82df7
Monikaa1909 <monikaa1909@gmail.com>
2022-04-26 21:47:34 +0200

    start part where block transformations by SnArray is making

--- src/components/DES.vue
+++ src/components/DES.vue
@@ -44,7 +44,7 @@ let dataBlock = [
     1, 1, 1, 1, 1, 0, 0, 1,
     1, 1, 1, 1, 1, 0, 1, 0
 ]
-let initialPermutation = [
+const initialPermutation = [
   58, 50, 42, 34, 26, 18, 10, 2,
   60, 52, 44, 36, 28, 20, 12, 4,
   62, 54, 46, 38, 30, 22, 14, 6,
@@ -54,7 +54,7 @@ let initialPermutation = [
   61, 53, 45, 37, 29, 21, 13, 5,
   63, 55, 47, 39, 31, 23, 15, 7
 ]
-let permutedChoice = [
+const permutedChoice = [
   57, 49, 41, 33, 25, 17, 9,
   1, 58, 50, 42, 34, 26, 18,
   10, 2, 59, 51, 43, 35, 27,
@@ -64,7 +64,7 @@ let permutedChoice = [
   14, 6, 61, 53, 45, 37, 29,
   21, 13, 5, 28, 20, 12, 4
 ]
-let permutedChoice2 = [
+const permutedChoice2 = [
   14, 17, 11, 24, 1, 5,
   3, 28, 15, 6, 21, 10,
   23, 19, 12, 4, 26, 8,
@@ -74,7 +74,7 @@ let permutedChoice2 = [
   44, 49, 39, 56, 34, 53,
   46, 42, 50, 36, 29, 32
 ]
-let shiftTableForEachIteration = [
+const shiftTableForEachIteration = [
   1, 1, 2, 2,
   2, 2, 2, 2,
   1, 2, 2, 2,
@@ -89,6 +89,61 @@ const extensionArray = [
   20, 21, 22, 23, 24, 25,
   24, 25, 26, 27, 28, 29,
   28, 29, 30, 31, 32, 1
+]
+const SnArray = [
+    // S1:
+    [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+      0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+      4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+      15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+    // S2:
+    [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+      3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+      0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+      13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9
+    ],
+    // S3:
+    [
+      10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+      13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+      13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+      1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12
+    ],
+    // S4:
+    [
+      7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+      13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+      10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+      3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14
+    ],
+    // S5:
+    [
+      2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+      14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+      4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+      11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3
+    ],
+    // S6:
+    [
+      12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+      10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+      9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+      4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13
+    ],
+    // S7:
+    [
+      4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+      13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+      1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+      6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12
+    ],
+    // S8:
+    [
+      13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+      1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+      7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+      2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11
+    ]
 ]
 
 function showBlock(block) {
@@ -111,13 +166,6 @@ function startDES(event){
   console.log("Blok po permutacji 'initial permutation': ")
   showBlock(dataBlock)
 
-  leftDataBlock = dataBlock.slice(0, dataBlock.length/2)
-  rightDataBlock = dataBlock.slice(dataBlock.length/2)
-  console.log("Lewy blok: ")
-  showBlock(leftDataBlock)
-  console.log("Prawy blok: ")
-  showBlock(rightDataBlock)
-
   console.log("Klucz początkowy: ")
   // zamieniam klucz na zwykłe liczby od 1 do 64 tylko po to żeby łatwiej sprawdzać poprawność permutacji
   // for(let i = 0; i < 64; i++) {
@@ -129,35 +177,48 @@ function startDES(event){
   console.log("Przetworzony klucz 56-bitowy permutacją 'permuted choice': ")
   showBlock(key)
 
-  CKeyBlock = key.slice(0, key.length/2)
-  DKeyBlock = key.slice(key.length/2)
-  console.log("Pierwsza część klucza C: ")
-  showBlock(CKeyBlock)
-  console.log("Druga część klucza D: ")
-  showBlock(DKeyBlock)
+  for (let i = 0; i < 1; i++) {
+    leftDataBlock = dataBlock.slice(0, dataBlock.length/2)
+    rightDataBlock = dataBlock.slice(dataBlock.length/2)
+    console.log("Lewy blok: ")
+    showBlock(leftDataBlock)
+    console.log("Prawy blok: ")
+    showBlock(rightDataBlock)
 
-  CKeyBlock = makeLeftShift(CKeyBlock, shiftTableForEachIteration[0])
-  console.log("Pierwsza część klucza C po 1. przesunięciu w lewo: ")
-  showBlock(CKeyBlock)
-  DKeyBlock = makeLeftShift(DKeyBlock, shiftTableForEachIteration[0])
-  console.log("Pierwsza część klucza D po 1. przesunięciu w lewo: ")
-  showBlock(DKeyBlock)
+    CKeyBlock = key.slice(0, key.length/2)
+    DKeyBlock = key.slice(key.length/2)
+    console.log("Pierwsza część klucza C: ")
+    showBlock(CKeyBlock)
+    console.log("Druga część klucza D: ")
+    showBlock(DKeyBlock)
 
-  key = CKeyBlock.concat(DKeyBlock)
-  console.log("Połączone części klucza C i D: ")
-  showBlock(key)
+    CKeyBlock = makeLeftShift(CKeyBlock, shiftTableForEachIteration[i])
+    console.log("Pierwsza część klucza C po 1. przesunięciu w lewo: ")
+    showBlock(CKeyBlock)
+    DKeyBlock = makeLeftShift(DKeyBlock, shiftTableForEachIteration[i])
+    console.log("Pierwsza część klucza D po 1. przesunięciu w lewo: ")
+    showBlock(DKeyBlock)
 
-  key = makePermutedChoice2(key)
-  console.log("Przetworzony klucz 48-bitowy permutacją 'permuted choice 2': ")
-  showBlock(key)
+    key = CKeyBlock.concat(DKeyBlock)
+    console.log("Połączone części klucza C i D: ")
+    showBlock(key)
 
-  rightDataBlock = makeExtensionArrayPermutation(rightDataBlock)
-  console.log("Przetworzona prawa część bloku wejściowego za pomocą 'extension array': ")
-  showBlock(rightDataBlock)
+    key = makePermutedChoice2(key)
+    console.log("Przetworzony klucz 48-bitowy permutacją 'permuted choice 2': ")
+    showBlock(key)
 
-  xorBlock = makeXORforBlocks(key, rightDataBlock)
-  console.log("48-bitowy ciąg uzyskany za pomocą operacji XOR na 48-bitowym kluczu i 48-bitowej wersji prawego bloku: ")
-  showBlock(xorBlock)
+    rightDataBlock = makeExtensionArrayPermutation(rightDataBlock)
+    console.log("Przetworzona prawa część bloku wejściowego za pomocą 'extension array': ")
+    showBlock(rightDataBlock)
+
+    xorBlock = makeXORforBlocks(key, rightDataBlock)
+    console.log("48-bitowy ciąg uzyskany za pomocą operacji XOR na 48-bitowym kluczu i 48-bitowej wersji prawego bloku: ")
+    showBlock(xorBlock)
+
+    xorBlock = makeBlockTransformationsBySnArray(xorBlock)
+    console.log("32-bitowy ciąg uzyskany za pomocą przekształceń za pomocą tablic S 48-bitowego ciągu uzyskanego po operacji XOR: ")
+    showBlock(xorBlock)
+  }
 }
 
 function makeInitialPermutation(block) {
@@ -235,6 +296,29 @@ function makeXORforBlocks(firstBlock, secondBlock) {
     xorBlock[i] = XOR(firstBlock[i], secondBlock[i])
   }
   return xorBlock
+}
+
+function makeBlockTransformationsBySnArray(block) {
+  let partsOfBlock = []
+  for (let i = 0; i < 8; i++ ) {
+    partsOfBlock[i] = block.slice(6 * i, (6 * i) + 6)
+  }
+
+  // Sprawdzam poprawność "pocięcia bloku"
+  console.log("Block: ")
+  showBlock(block)
+  console.log("Pociety blok:")
+  for (let i = 0; i < 8; i++ ) {
+    showBlock(partsOfBlock[i])
+  }
+
+  let rowNr, colNr
+  for (let i = 0; i < 1; i++) {
+    rowNr = parseInt(partsOfBlock[i][0] + "" + partsOfBlock[i][5], 2)
+    console.log("row nr in binary: " + partsOfBlock[i][0] + partsOfBlock[i][5] + " and in decimal " + rowNr)
+  }
+
+  return block
 }
 
 function onFileSelected(event) {
